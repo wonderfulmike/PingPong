4ca678d6f41f1ab5ba6cf2f80672184fe8f0757f
wonderfulmike <mike76231@gmail.com>
2018-06-29 12:03:21 +0300

    Added jQuery script and run a prompt-working

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//backend
+var input = prompt("Input your number...");
+
+
+if (input % 3 == 0) {
+  alert("ping");
+} else {
+  if (input % 5 == 0) {
+    alert("pong");
+  }
+
+
+
+
+
+
+
+
+//frontend
