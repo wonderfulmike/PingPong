f627c0d815acd32776156167d428df9e1d032040
wonderfulmike <mike76231@gmail.com>
2018-07-02 08:44:25 +0300

    Added new classes on html

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//Business Logic
+
+/*let PingPongs = function (num) {
+
+if (50 % 15 == 0) {
+  return("pingpong");
+} else if (50 % 5 == 0){
+  return("pong");
+} else if (50 % 3 == 0) {
+  return("ping");
+} else {
+  prompt("Enter a new number");
+}
+};*/
+
+//
+
+var inputs = [];
+inputs.push($("getElementById('the-input')").val());
+return inputs;
